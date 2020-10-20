6b475249be8a998b20d22dcfe8023309a700ef2d
azizidomar <47518542+azizidomar@users.noreply.github.com>
2020-10-20 09:08:34 +0200

    add vga

---
+++ vga/colours.vhd
@@ -0,0 +1,96 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity colours is
+	generic(
+		DATA_LENGTH	: integer := 16
+	);
+	port(
+		reset		: in  std_logic;
+		clk			: in  std_logic;
+		
+		count_v		: in integer range 0 to 1023;        
+		count_h   	: in integer range 0 to 1023;
+		
+		red			: out std_logic_vector((DATA_LENGTH-1) downto 0); 
+		green		: out std_logic_vector((DATA_LENGTH-1) downto 0); 
+		blue     	: out std_logic_vector((DATA_LENGTH-1) downto 0)
+	);        
+		
+end entity colours;
+
+
+architecture arc_colours of colours is
+begin
+	process(clk,reset)
+	begin  
+    
+		if reset = '1' then
+		  red 	<= x"0";
+		  green <= x"0";
+		  blue 	<= x"0"; 
+		elsif rising_edge(clk)then
+			
+			if( ((count_v >= 0 and count_v < 8) and (count_v > 791 and count_v <= 799 )) or ((count_h >= 0 and count_h < 2) and (count_h > 516 and count_h <= 524 )) ) then
+				red 	<= x"0";
+				green 	<= x"0";
+				blue	<= x"0"; 
+			elsif( (count_h >= 104 and count_h < 152)  and (not(count_v >= 2 and count_v < 4   )) ) then 
+				red 	<= x"0";
+				green 	<= x"0";
+				blue 	<= x"0"; 
+			elsif( (count_v >= 4   and count_v < 37 )  and (not(count_h >= 8 and count_h < 104 )) ) then 
+				red 	<= x"0";
+				green 	<= x"0";
+				blue 	<= x"0"; 
+			elsif( (count_v >= 37  and count_v < 517)  and (count_h >= 152 and count_h < 791) ) then 
+			
+				-- NOIR
+				if( (count_v >= 152 and count_v < 232) ) then 
+					red 	<= x"0";
+					green 	<= x"0";
+					blue 	<= x"0"; 
+				-- ROUGE
+				elsif( (count_v >= 232 and count_v < 312) )then 
+					red 	<= x"F";
+					green 	<= x"0";
+					blue 	<= x"0"; 
+				-- VERT
+				elsif( (count_v >= 312 and count_v < 392) ) then 
+					red 	<= x"0";
+					green 	<= x"F";
+					blue 	<= x"0"; 
+				-- JAUNE
+				elsif( (count_v >= 392 and count_v < 472) )then 
+					red 	<= x"F";
+					green 	<= x"F";
+					blue 	<= x"0"; 
+				-- BLEU
+				elsif( (count_v >= 472 and count_v < 552) ) then 
+					red 	<= x"0";
+					green	<= x"0";
+					blue 	<= x"F"; 
+				-- ROSE
+				elsif( (count_v >= 552 and count_v < 632) )then 
+					red 	<= x"F";
+					green 	<= x"B";
+					blue 	<= x"C"; 
+				-- BLEU CLAIR
+				elsif( (count_v >= 632 and count_v < 712) )then 
+					red 	<= x"0";
+					green 	<= x"0";
+					blue 	<= x"0"; 
+				-- BLANC
+				elsif( (count_v >= 712 and count_v < 792) )then 
+					red 	<= x"8";
+					green 	<= x"F";
+					blue 	<= x"E"; 
+				
+				end if;
+				
+			end if;
+				
+		end if;
+	end process;
+end architecture;
---
+++ vga/vga_controller.vhd
@@ -0,0 +1,73 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity vga_controller is 
+	generic(
+		FREQ_CLK	:	INTEGER := 50000000;
+		DATA_LENGTH	:	INTEGER := 16
+	);
+	port(
+		reset		: in  std_logic;
+		clk			: in  std_logic;   
+		
+		ver_sync	: out std_logic;
+		hor_sync	: out std_logic;
+	
+		red			: out std_logic_vector((DATA_LENGTH-1) downto 0);  
+		green		: out std_logic_vector((DATA_LENGTH-1) downto 0); 
+		blue		: out std_logic_vector((DATA_LENGTH-1) downto 0)
+	);
+end entity vga_controller;
+
+
+architecture arc_vga_controller of vga_controller is
+   signal count_v, count_h 	: integer range 0 to 1023;
+   signal reg_hor_sync, reg_ver_sync : std_logic;
+
+begin
+
+  process(clk,reset)
+  begin  
+    
+    if reset = '1'  then
+      count_v <= 0;
+      count_h <= 0;
+      reg_hor_sync <= '1';
+      reg_ver_sync <= '1';
+            
+ 	elsif rising_edge(clk)then
+     
+		if(count_h = 799 ) then 
+			count_h <= 0;
+			count_v <= count_v + 1;
+			if(count_v = 524) then
+				count_v <= 0;
+			end if;
+		else 
+			count_h <= count_h + 1;
+		end if;
+		
+		if( count_h < 8 )then
+			reg_hor_sync <= '1';
+		elsif ( (count_h >= 8  ) and (count_h < 104) ) then
+			reg_hor_sync <= '0';
+		elsif ( (count_h >= 104) and (count_h < 800) ) then
+			reg_hor_sync <= '1';
+		end if;
+		
+		if( count_v < 2 )then
+			reg_ver_sync <= '1';
+		elsif ( (count_v >= 2  ) and (count_v < 4  ) ) then
+			reg_ver_sync <= '0';
+		elsif ( (count_v >= 4  ) and (count_v < 525) ) then
+			reg_ver_sync <= '1';
+		end if;
+      
+    end if;
+  end process;
+  
+  U0_COLOURS : entity work.colours  port map(reset => reset,clk => clk,count_v => count_v,count_h => count_h,red => red,green => green, blue => blue);
+
+	 
+end architecture arc_vga_controller;
