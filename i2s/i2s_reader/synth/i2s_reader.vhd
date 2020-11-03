06115ba9309b1ba29e84e8639f822f34eb0c2638
thomasbouix <thomas.bouix@etu.upmc.fr>
2020-11-03 10:53:18 +0100

    commentaires i2s_reader.vhd

--- i2s/i2s_reader/synth/i2s_reader.vhd
+++ i2s/i2s_reader/synth/i2s_reader.vhd
@@ -1,11 +1,14 @@
+-- I2S Reader en mode esclave + receiver
+-- Lit la sortie de l'ADC et la formatte sur DATA_LENGTH bits
+
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity i2s_reader is 
 	generic(
-		MCLK_FREQ	:	INTEGER := 22579200;
-		SCLK_FREQ	:	INTEGER := 2822400;
-		LRCK_FREQ	:	INTEGER	:= 44100;
+		MCLK_FREQ	:	INTEGER := 22579200;	-- mclk/sclk = 8
+		SCLK_FREQ	:	INTEGER := 2822400;	-- sclk/lrck = 64 
+		LRCK_FREQ	:	INTEGER	:= 44100;	-- frequence d'echantillonage 
 		DATA_LENGTH	:	INTEGER	:= 16
 	);
 	port(
@@ -13,10 +16,10 @@ entity i2s_reader is
 		clk	:	in	std_logic;
 		data	:	out	std_logic_vector((DATA_LENGTH-1) downto 0);
 		
-		mclk	:	out	std_logic;
-		sclk	:	out 	std_logic;
-		lrck	:	out 	std_logic;
-		din	:	in 	std_logic
+		mclk	:	out	std_logic;		-- clock du systeme
+		sclk	:	out 	std_logic;		-- frequence din
+		lrck	:	out 	std_logic;		-- left / right : change tout les (DATA_LENGTH+2) * SCLK
+		din	:	in 	std_logic		-- sortie de l'ADC
 	);
 end entity i2s_reader;
 
