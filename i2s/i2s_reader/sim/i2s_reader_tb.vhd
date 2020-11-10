092c186c3aa5f1c2ea26c7a969dda0b12ea1cf2b
thomasbouix <thomas.bouix@etu.upmc.fr>
2020-11-10 09:59:17 +0100

    debugging i2s tb

--- i2s/i2s_reader/sim/i2s_reader_tb.vhd
+++ i2s/i2s_reader/sim/i2s_reader_tb.vhd
@@ -2,62 +2,50 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity i2s_reader_tb is
-	generic(
-			MCLK_FREQ	:	INTEGER := 22579200;	-- mclk/sclk = 8
-			SCLK_FREQ	:	INTEGER := 2822400;	-- sclk/lrck = 64 
-			LRCK_FREQ	:	INTEGER	:= 44100;	-- frequence d'echantillonage 
-			DATA_LENGTH	:	INTEGER	:= 16		-- taille des données de l'ADC
-	);
-
 end i2s_reader_tb;
 
 architecture arc_i2s_reader_tb of i2s_reader_tb is
-    signal reset, clk, mclk, sclk, lrck, din : std_logic; 
-    signal data : std_logic_vector(15 downto 0);
+	signal reset, clk, mclk, sclk, lrck, din : std_logic; 
+	signal data : std_logic_vector(15 downto 0);
+	constant clock_t : time :=  44.288941 ns;
 begin
 
-    i2s_reader : entity work.i2s_reader 
-    	generic map(MCLK_FEQ => MCLK_FREQ, SCLK_FREQ => SCLK_FREQ, LRCK_FREQ => LRCK_FREQ, DATA_LENGTH => DATA_LENGTH)
-    	port map (reset => reset, clk => clk, data => data, mclk => mclk, sclk => sclk, lrck=>lrck, din=>din);
+	i2s_reader : entity work.i2s_reader 
+    		port map (reset => reset, clk => clk, data => data, mclk => mclk, sclk => sclk, lrck=>lrck, din=>din);
 	
-	process
-	begin
+	rst_p : process begin
 		reset <= '1';
-		wait for 44.288941 ns;
+		wait for clock_t;
 		reset <= '0';
 		wait for 50 us;
-
 		wait;
 	end process;
 	
-	process
-	begin
-	
+	clock : process begin
 		clk <= '0';
-		wait for 1/MCLK_FREQ ns;
+		wait for clock_t;
 		clk <= '1';
-		wait for 1/MCLK_FREQ ns;
+		wait for clock_t;
+	end process clock;
 	
-	end process;
-	
-	process
-	begin
-	
-		wait for 1/SCLK_FREQ ns;
+	data_in : process begin
+		wait until rising_edge(sclk);
 		din <= '0';
-		wait for 1/SCLK_FREQ ns;
+		wait until rising_edge(sclk);
 		din <= '1';
-		wait for 1/SCLK_FREQ ns;
+		wait until rising_edge(sclk);
 		din <= '0';
-		wait for 1/SCLK_FREQ ns;
+		wait until rising_edge(sclk);
+		din <= '1';
+		wait until rising_edge(sclk);
+		din <= '1';
+		wait until rising_edge(sclk);
 		din <= '0';
-		wait for 1/SCLK_FREQ ns;
-		din <= '1';
-		wait for 1/SCLK_FREQ ns;
-		din <= '1';
-		wait for 1/SCLK_FREQ ns;
-		
-	end process;
+		wait until rising_edge(sclk);
+		din <= '0';
+		wait until rising_edge(sclk);
+		din <= '0';
+	end process data_in;
 	
 	
 end architecture arc_i2s_reader_tb ;
