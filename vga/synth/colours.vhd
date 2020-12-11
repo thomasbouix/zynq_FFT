6d22a2f566a8675cedd061497fd369fc2da38b5e
azizidomar <aziz.idomar@etu.upmc.fr>
2020-12-11 16:51:07 +0100

    modif vga_controller

--- vga/synth/colours.vhd
+++ vga/synth/colours.vhd
@@ -4,93 +4,61 @@ use ieee.numeric_std.all;
 
 entity colours is
 	generic(
-		DATA_LENGTH	: integer := 4
+		-- VGA Signal 640 x 480 @ 60 Hz Industry standard timing
+
+		FREQ_CLK	: INTEGER := 25000000;	-- Frequency
+		DATA_LENGTH	: INTEGER := 4;	-- Data length
+
+		HOR_VA		: INTEGER := 640;	-- Horizontal visible area
+		HOR_FP		: INTEGER := 16; 	-- Horizontal front porch
+		HOR_SP		: INTEGER := 96;	-- Horizontal sync pulse
+		HOR_BP		: INTEGER := 48;	-- Horizontal back porch
+
+		VER_VA		: INTEGER := 480;	-- Vertical visible area
+		VER_FP		: INTEGER := 10;	-- Vertical front porch
+		VER_SP		: INTEGER := 2;	-- Vertical sync pulse
+		VER_BP		: INTEGER := 33	-- Vertical back porch
 	);
 	port(
-		reset_n		: in  std_logic;
+		reset_n	: in  std_logic;
 		clk		: in  std_logic;
-		
-		count_v		: in integer range 0 to 1023;        
+
+		count_v	: in integer range 0 to 1023;
 		count_h   	: in integer range 0 to 1023;
-		
-		red		: out std_logic_vector((DATA_LENGTH-1) downto 0); 
-		green		: out std_logic_vector((DATA_LENGTH-1) downto 0); 
+
+		red		: out std_logic_vector((DATA_LENGTH-1) downto 0);
+		green		: out std_logic_vector((DATA_LENGTH-1) downto 0);
 		blue     	: out std_logic_vector((DATA_LENGTH-1) downto 0)
-	);        
-		
+	);
+
 end entity colours;
 
 
 architecture arc_colours of colours is
+
+constant n_mire : integer := HOR_VA/8;
+
 begin
-	process(clk,reset_n)
-	begin  
-    
-		if reset_n = '1' then
-		  red 	<= x"0";
-		  green <= x"0";
-		  blue 	<= x"0"; 
-		elsif rising_edge(clk)then
-			
-			if( ((count_v >= 0 and count_v < 8) and (count_v > 791 and count_v <= 799 )) or ((count_h >= 0 and count_h < 2) and (count_h > 516 and count_h <= 524 )) ) then
-				red 	<= x"0";
-				green 	<= x"0";
-				blue	<= x"0"; 
-			elsif( (count_h >= 104 and count_h < 152)  and (not(count_v >= 2 and count_v < 4   )) ) then 
-				red 	<= x"0";
-				green 	<= x"0";
-				blue 	<= x"0"; 
-			elsif( (count_v >= 4   and count_v < 37 )  and (not(count_h >= 8 and count_h < 104 )) ) then 
-				red 	<= x"0";
-				green 	<= x"0";
-				blue 	<= x"0"; 
-			elsif( (count_v >= 37  and count_v < 517)  and (count_h >= 152 and count_h < 791) ) then 
-			
-				-- NOIR
-				if( (count_v >= 152 and count_v < 232) ) then 
-					red 	<= x"0";
-					green 	<= x"0";
-					blue 	<= x"0"; 
-				-- ROUGE
-				elsif( (count_v >= 232 and count_v < 312) )then 
-					red 	<= x"F";
-					green 	<= x"0";
-					blue 	<= x"0"; 
-				-- VERT
-				elsif( (count_v >= 312 and count_v < 392) ) then 
-					red 	<= x"0";
-					green 	<= x"F";
-					blue 	<= x"0"; 
-				-- JAUNE
-				elsif( (count_v >= 392 and count_v < 472) )then 
-					red 	<= x"F";
-					green 	<= x"F";
-					blue 	<= x"0"; 
-				-- BLEU
-				elsif( (count_v >= 472 and count_v < 552) ) then 
-					red 	<= x"0";
-					green	<= x"0";
-					blue 	<= x"F"; 
-				-- ROSE
-				elsif( (count_v >= 552 and count_v < 632) )then 
-					red 	<= x"F";
-					green 	<= x"B";
-					blue 	<= x"C"; 
-				-- BLEU CLAIR
-				elsif( (count_v >= 632 and count_v < 712) )then 
-					red 	<= x"0";
-					green 	<= x"0";
-					blue 	<= x"0"; 
-				-- BLANC
-				elsif( (count_v >= 712 and count_v < 792) )then 
-					red 	<= x"8";
-					green 	<= x"F";
-					blue 	<= x"E"; 
-				
-				end if;
-				
-			end if;
-				
-		end if;
-	end process;
+    process(clk,reset_n)
+    begin
+
+    if reset_n = '1' then
+        red   <= (others=>'0');
+        green <= (others=>'0');
+        blue  <= (others=>'0');
+
+    elsif rising_edge(clk)then
+
+	if( (count_h < HOR_VA) and (count_v < VER_VA) ) then
+	    red   <= (others=>'1');
+	    green <= (others=>'1');
+	    blue  <= (others=>'1');
+	else
+	    red   <= (others=>'0');
+	    green <= (others=>'0');
+	    blue  <= (others=>'0');
+	end if;
+
+    end if;
+    end process;
 end architecture;
