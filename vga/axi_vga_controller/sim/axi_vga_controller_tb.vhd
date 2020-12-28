511b767b1ab61d3c0ca7d27fa4c111f0c44c9b61
azizidomar <aziz.idomar@etu.upmc.fr>
2020-12-28 17:06:16 +0100

    add axi_vga_controller

---
+++ vga/axi_vga_controller/sim/axi_vga_controller_tb.vhd
@@ -0,0 +1,100 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity axi_vga_controller_tb is
+
+end entity;
+
+architecture arc_axi_vga_controller_tb of axi_vga_controller_tb is
+  signal aresetn, aclk  : std_logic;
+  signal s_axis_tdata   : std_logic_vector(31 downto 0);
+  signal s_axis_tkeep  	: std_logic_vector(3  downto 0);
+  signal s_axis_tvalid	: std_logic;
+  signal s_axis_tready  : std_logic;
+  signal s_axis_tlast   : std_logic;
+
+  signal vga_clk        : std_logic;
+  signal ver_sync       : std_logic;
+  signal hor_sync       : std_logic;
+  signal red            : std_logic_vector(3 downto 0);
+  signal green          : std_logic_vector(3 downto 0);
+  signal blue           : std_logic_vector(3 downto 0);
+
+  constant clock_axi    : time :=  2 ns;
+  constant clock_vga    : time :=  19.86 ns;
+
+  constant pkt_len      : integer := 640;
+
+begin
+
+    axi_vga_controller : entity work.axi_vga_controller port map (
+                                                                    aresetn       => aresetn,
+                                                                    aclk          => aclk,
+                                                                    s_axis_tdata  => s_axis_tdata,
+                                                                    s_axis_tkeep  => s_axis_tkeep,
+                                                                    s_axis_tlast  => s_axis_tlast,
+                                                                    s_axis_tready => s_axis_tready,
+                                                                    s_axis_tvalid => s_axis_tvalid,
+                                                                    vga_clk       => vga_clk,
+                                                                    ver_sync      => ver_sync,
+                                                                    hor_sync      => hor_sync,
+                                                                    red           => red,
+                                                                    green         => green,
+                                                                    blue          => blue
+                                                                 );
+
+
+  clock_vga_p : process
+  begin
+     vga_clk <= '0';
+     wait for clock_vga;
+     vga_clk <= '1';
+     wait for clock_vga;
+  end process clock_vga_p;
+
+  clock_axi_p : process begin
+    aclk <= '0';
+    wait for clock_axi;
+    aclk <= '1';
+    wait for clock_axi;
+  end process;
+
+  aresetn_p : process begin
+      aresetn <= '0';
+      wait for 10*clock_axi;
+      aresetn <= '1';
+      wait for 50 us;
+      wait;
+  end process;
+
+
+  transaction_p : process
+  --variable index : integer := 0;
+  begin
+      wait for 2000*clock_axi;
+      s_axis_tdata  <= x"00000000";
+      s_axis_tkeep  <= x"F";
+      s_axis_tlast  <= '0';
+      s_axis_tvalid <= '1';
+
+      wait until s_axis_tready = '1';
+
+      for index in 1 to (pkt_len - 1) loop
+        wait for 2*clock_axi;
+        s_axis_tdata  <= std_logic_vector(to_unsigned(index, s_axis_tdata'length));
+      end loop;
+
+      wait for 2*clock_axi;
+      s_axis_tdata  <= std_logic_vector(to_unsigned((pkt_len - 1), s_axis_tdata'length));
+      s_axis_tlast  <= '1';
+
+      wait for 2*clock_axi;
+      s_axis_tlast  <= '0';
+      s_axis_tvalid <= '0';
+      s_axis_tdata  <= x"00000000";
+
+      wait for 40*clock_axi;
+  end process;
+
+end arc_axi_vga_controller_tb;
---
+++ vga/axi_vga_controller/synth/axi_vga_controller.vhd
@@ -0,0 +1,189 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity axi_vga_controller is
+	generic(
+		-- AXI Stream
+	  DATA_LEN     : INTEGER := 32;
+		PKT_LEN      : INTEGER := 640;
+
+		-- VGA Signal 640 x 480 @ 60 Hz Industry standard timing
+		FREQ_CLK	   : INTEGER := 25175000;	  -- Frequency
+		PIXEL_LEN    : INTEGER := 4;	        -- Data length
+
+		HOR_VA		   : INTEGER := 640;	      -- Horizontal visible area
+		HOR_FP		   : INTEGER := 16; 	      -- Horizontal front porch
+		HOR_SP		   : INTEGER := 96;	        -- Horizontal sync pulse
+		HOR_BP		   : INTEGER := 48;	        -- Horizontal back porch
+
+		VER_VA		   : INTEGER := 480;	      -- Vertical visible area
+		VER_FP		   : INTEGER := 10;	        -- Vertical front porch
+		VER_SP		   : INTEGER := 2;	        -- Vertical sync pulse
+		VER_BP		   : INTEGER := 33	        -- Vertical back porch
+	);
+	port(
+		-- AXI signals
+		aresetn	        : in  std_logic;
+		aclk		        : in  std_logic;
+
+		s_axis_tdata  	: in   std_logic_vector((DATA_LEN - 1) downto 0);
+		s_axis_tkeep 	  : in   std_logic_vector(((DATA_LEN / 8) - 1) downto 0);
+		s_axis_tlast 	  : in   std_logic;
+		s_axis_tready 	: out  std_logic;
+		s_axis_tvalid 	: in   std_logic;
+
+		-- VGA signals
+		vga_clk		      : in  std_logic;
+
+		ver_sync	      : out std_logic;
+		hor_sync	      : out std_logic;
+
+		red		          : out std_logic_vector((PIXEL_LEN-1) downto 0);
+		green		        : out std_logic_vector((PIXEL_LEN-1) downto 0);
+		blue		        : out std_logic_vector((PIXEL_LEN-1) downto 0)
+	);
+end entity axi_vga_controller;
+
+
+architecture arc_axi_vga_controller of axi_vga_controller is
+
+	constant h_period	   : integer := HOR_VA + HOR_FP + HOR_SP + HOR_BP;
+	signal count_h	     : integer range 0 to (h_period - 1);
+
+	constant v_period    : integer := VER_VA + VER_FP + VER_SP + VER_BP;
+	signal count_v	     : integer range 0 to (v_period - 1);
+
+	signal va_state	     : std_logic;
+	signal va_state_old	 : std_logic;
+	signal va_state_cur	 : std_logic;
+
+	signal counter_pixel : integer range 0 to PKT_LEN;
+
+	type t_axi_state is (WAITREADY, WAITVALID, ACQUIRE, TREAT);
+	signal axi_state : t_axi_state;
+
+	type t_datas is array ((PKT_LEN-1) downto 0) of std_logic_vector(31 downto 0);
+	signal datas : t_datas;
+
+	signal counter_data	 : integer range 0 to PKT_LEN;
+	signal reg_data      : std_logic_vector((DATA_LEN-1) downto 0);
+
+begin
+
+  process(vga_clk, aresetn)
+  begin
+  	if aresetn = '0'  then
+		count_v       <= 0;
+		count_h       <= 0;
+		counter_pixel <= 0;
+
+		va_state      <= '0';
+		va_state_old  <= '0';
+    va_state_cur  <= '1';
+
+		hor_sync      <= '1';
+		ver_sync      <= '1';
+
+ 		elsif rising_edge(vga_clk)then
+
+			va_state_old <= va_state_cur;
+      va_state_cur <= va_state;
+
+			if(count_h = (h_period - 1)) then
+				count_h <= 0;
+				if(count_v = (v_period - 1)) then
+					count_v <= 0;
+				else
+					count_v <= count_v + 1;
+				end if;
+			else
+				count_h <= count_h + 1;
+			end if;
+
+			if ( (count_h > (HOR_FP + HOR_VA)) and (count_h < (HOR_FP + HOR_VA + HOR_SP)) ) then
+				hor_sync <= '0';
+			else
+				hor_sync <= '1';
+			end if;
+
+			if ( (count_v > (VER_FP + VER_VA)) and (count_v < (VER_FP + VER_VA + VER_SP)) ) then
+				ver_sync <= '0';
+			else
+				ver_sync <= '1';
+			end if;
+
+			if( (count_h >= HOR_FP) and (count_h <= (HOR_FP + HOR_VA)) and (count_v >= VER_FP) and (count_v <= (VER_FP + VER_VA)) ) then
+				va_state      <= '1';
+
+				red           <= datas(counter_pixel)((PIXEL_LEN-1) downto 0);
+				green         <= datas(counter_pixel)((PIXEL_LEN-1) downto 0);
+				blue          <= datas(counter_pixel)((PIXEL_LEN-1) downto 0);
+
+				counter_pixel <= counter_pixel + 1;
+			else
+				va_state      <= '0';
+
+				red           <= (others => '0');
+				green         <= (others => '0');
+				blue          <= (others => '0');
+
+				counter_pixel <= 0;
+			end if;
+		end if;
+  end process;
+
+	process(aclk, aresetn)
+	begin
+		if aresetn = '0' then
+			counter_data  <= 0;
+			s_axis_tready <= '0';
+
+			axi_state     <= WAITREADY;
+		elsif aclk'event and aclk='1' then
+			case axi_state is
+				when WAITREADY =>
+					counter_data <= 0;
+					reg_data     <= s_axis_tdata;
+
+					if(va_state_old = '0' and va_state_cur = '1') then
+						s_axis_tready <= '1';
+
+						if(s_axis_tvalid = '1') then
+							axi_state <= ACQUIRE;
+						else
+							axi_state <= WAITVALID;
+						end if;
+					else
+						s_axis_tready <= '0';
+						axi_state     <= WAITREADY;
+					end if;
+				when WAITVALID =>
+					reg_data <= s_axis_tdata;
+
+					if(s_axis_tvalid = '1') then
+						axi_state           <= ACQUIRE;
+					else
+						axi_state           <= WAITVALID;
+					end if;
+				when ACQUIRE =>
+					if(counter_data = PKT_LEN) then
+						s_axis_tready       <= '0';
+						reg_data            <= (others => '0');
+
+						counter_data        <= 0;
+						axi_state           <= WAITREADY;
+					else
+						s_axis_tready       <= '1';
+						reg_data            <= s_axis_tdata;
+						datas(counter_data) <= reg_data;
+						counter_data        <= counter_data + 1;
+						axi_state           <= ACQUIRE;
+					end if;
+				when others =>
+					axi_state <= WAITREADY;
+			end case;
+		else end if;
+	end process;
+
+end architecture arc_axi_vga_controller;
