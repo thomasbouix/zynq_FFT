a7c128c8cb5c6eb1d61418738f4a084175c91ce8
thomasbouix <thomas.bouix@etu.upmc.fr>
2021-01-12 09:52:14 +0100

    user app is compiled

--- os/impl/package/user_app/app/user_app.c
+++ os/impl/package/user_app/app/user_app.c
@@ -8,7 +8,7 @@
 #include <sys/ioctl.h>
 #include <string.h>
 
-#include "my_macro.h"
+#include "ioctl.h"
 
 #define BUFFER_LENGTH	10
 
@@ -20,9 +20,11 @@ int main(int argc, char * argv[]) {
 
 	if (argc != 2) {
 		printf("Missing arg :\n			\
-			\t1 : MY_DRIVER_PRINT\n		\
-			\t2 : DMA_READ_S2MM\n		\
-			\t3 : DMA_IOWRITE32_TEST\n	\
+			\t0 : AXI_DMA_INIT\n		\
+			\t1 : AXI_DMA_START\n		\
+			\t2 : AXI_DMA_STOP\n		\
+			\t3 : AXI_DMA_WAIT\n		\
+			\t4 : AXI_DMA_SWAP\n		\
 			Terminating\n");
 		return 0;
 	}
@@ -38,21 +40,25 @@ int main(int argc, char * argv[]) {
 
 	/*-------------------IOCTL-------------------*/
 
-	p_axi_dma_buffer pbuffer;
-	pbuffer->address = (void*) malloc(BUFFER_LENGTH * sizeof(int));
-	pbuffer->length  = (size_t) BUFFER_LENGTH;	
-	
-	if 	( strcmp(argv[1], "1") == 0 ) {
-		printf("USER_APP : DMA_PRINT\n");	
-		ioctl(file, DMA_PRINT, NULL);
+	if 	( strcmp(argv[1], "0") == 0 ) {
+		printf("USER_APP : AXI_DMA_INIT\n");	
+		ioctl(file, AXI_DMA_INIT, NULL);
+	} 
+	else if ( strcmp(argv[1], "1") == 0 ) {
+		printf("USER_APP : AXI_DMA_START");	
+		ioctl(file, AXI_DMA_START, NULL);
 	} 
 	else if ( strcmp(argv[1], "2") == 0 ) {
-		printf("USER_APP : DMA_READ_S2MM");	
-		ioctl(file, DMA_READ_S2MM, pbuffer);
-	} 
+		printf("USER_APP : AXI_DMA_STOP\n");	
+		ioctl(file, AXI_DMA_STOP, NULL);
+	}
 	else if ( strcmp(argv[1], "3") == 0 ) {
-		printf("USER_APP : DMA_IOWRITE32_TEST\n");	
-		ioctl(file, DMA_IOWRITE32_TEST, NULL);
+		printf("USER_APP : AXI_DMA_WAIT\n");	
+		ioctl(file, AXI_DMA_WAIT, NULL);
+	}
+	else if ( strcmp(argv[1], "4") == 0 ) {
+		printf("USER_APP : AXI_DMA_SWAP\n");	
+		ioctl(file, AXI_DMA_SWAP, NULL);
 	}
 
 	close(file);
